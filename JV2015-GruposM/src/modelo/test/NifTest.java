d4dccc0022b329ac50e6f874b0fd6fbea4b921cb
ajp <ajp@TEMIS.alamo.local>
2016-05-08 14:45:13 +0200

    Añade NifTest

---
+++ JV2015-GruposM/src/modelo/test/NifTest.java
@@ -0,0 +1,39 @@
+package modelo.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NifTest {
+
+	@Test
+	public void testNifString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNif() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testNifNif() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTexto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetTexto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
